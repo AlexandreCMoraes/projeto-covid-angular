1128e652ea2e51eb444ee21c1934d5bec38380e4
AlexandreCMoraes <alexcmoraes@hotmail.com>
2021-06-07 21:27:03 -0300

    estilizaçao de txts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-covid';
+  
+  public routeLoading: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.routeLoading = true;
+      }
+
+      if (event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError) 
+      {
+          this.routeLoading = false;
+      }
+    });
+  }
+  
 }
